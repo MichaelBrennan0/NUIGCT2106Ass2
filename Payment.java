e3e69fc903f8fedfdc2bd6b56a648bb9e9b46dca
Micahel Brennan <mbrennan566@gmail.com>
2023-10-04 16:08:50 +0100

    3

---
+++ Payment.java
@@ -0,0 +1,21 @@
+/**
+ * Write a description of class Payment here.
+ *
+ * @author (your name)
+ * @version 1.0     4th Oct
+ */
+public class Payment
+{
+private int cardNumber;
+private int csv;
+private String cardName;
+
+public Payment(int cardNumber ,int csv, String cardName){
+this.cardNumber = cardNumber;
+this.csv = csv;
+this.cardName = cardName;
+
+}   
+    
+    
+}
